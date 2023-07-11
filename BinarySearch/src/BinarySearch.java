8d8aa657fef55d3d1acda6b10b031b01652bf7fc
Arthur Rodrigues <117749096+arthurrodrigues-dev@users.noreply.github.com>
2023-07-11 18:23:08 -0300

    binary search algorithm

---
+++ BinarySearch/src/BinarySearch.java
@@ -0,0 +1,21 @@
+public class BinarySearch {
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        int mid;
+
+        while (start <= end) {
+            mid = (start + end) / 2;
+
+            if (arr[mid] > target) {
+                end = mid - 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
