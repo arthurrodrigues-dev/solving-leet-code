23820d2251b1510675108a630e71f6f95ea4ee53
Arthur Rodrigues <117749096+arthurrodrigues-dev@users.noreply.github.com>
2023-07-12 00:18:59 -0300

    ceiling of a number

---
+++ BinarySearch/src/CeilingOfANumber.java
@@ -0,0 +1,32 @@
+public class CeilingOfANumber {
+    public static void main(String[] args) {
+       int[] arr = {2, 3, 5, 9, 14, 16, 17};
+       int target = 18;
+       int ans = ceiling(arr, target);
+       System.out.println(ans);
+    }
+
+    static int ceiling(int[] arr, int target) {
+
+        if (target > arr[arr.length - 1]) {
+            return -1;
+        }
+
+        int start = 0;
+        int end = arr.length - 1;
+        int mid;
+
+        while (start <= end) {
+            mid = (start + end) / 2;
+
+            if (arr[mid] > target) {
+                end = mid - 1;
+            } else if (arr[mid] < target){
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
